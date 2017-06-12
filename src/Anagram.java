4bbf5128d9b82cfb744bb69d7019f769b02fa1cb
norbert barics <norbert.barics@gmail.com>
2017-06-12 16:18:13 +0200

    anagram method repaired

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,6 +1,19 @@
 public class Anagram {
 
   public boolean isAnagram(String one, String two) {
-    return true;
+    int count = 0;
+    if (one.length() == two.length()) {
+
+      for (int i = 0; i < one.length(); ++i) {
+        if (two.contains(one.substring(i, i + 1))) {
+          count++;
+        }
+      }
+    }
+    if (count == one.length()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
