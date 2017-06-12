4b86917a0eefefd837e2eefc4b8053fe2c723b62
Balazs Stegmar <stegmarb@gmail.com>
2017-06-12 17:00:47 +0200

    fizzbuzz method is fully working

--- src/PrintFizzBuzz.java
+++ src/PrintFizzBuzz.java
@@ -6,6 +6,19 @@ public class PrintFizzBuzz {
   List<String> list = new ArrayList<>(Arrays.asList("is it working", "or not", "Fizz"));
 
   public List<String> fizzBuzz(){
+    for (int i = 1; i < 101; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        list.add("FizzBuzz");
+      } else {
+        if (i % 3 == 0) {
+          list.add("Fizz");
+        } else if (i % 5 == 0) {
+          list.add("Buzz");
+        } else {
+          list.add(Integer.toString(i));
+        }
+      }
+    }
     return list;
   }
 }
