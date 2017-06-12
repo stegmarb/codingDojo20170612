22c927e4a8e9094cb8b32015fe873f6170b3989b
norbert barics <norbert.barics@gmail.com>
2017-06-12 16:07:38 +0200

    anagram test added

--- test/GetIndexTest.java
+++ test/GetIndexTest.java
@@ -7,7 +7,12 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class GetIndexTest {
+
   private List<Integer> testList = new ArrayList<>();
+
+  GetIndex index = new GetIndex();
+
+  Anagram anagram = new Anagram();
 
   @Before
   public void createList() {
@@ -16,7 +21,21 @@ public class GetIndexTest {
 
   @Test
   public void testGetIndex() {
-    GetIndex index = new GetIndex();
+
     assertEquals(index.getIndex(0, testList), -1);
+  }
+
+  @Test
+  public void testValidNumberInList() {
+    assertEquals(index.getIndex(1, testList), 0);
+  }
+
+  @Test
+  public void testIsAnagram(){
+
+    String firstString = "abcd";
+    String secondString = "dcba";
+
+    assertTrue(anagram.isAnagram(firstString, secondString));
   }
 }
